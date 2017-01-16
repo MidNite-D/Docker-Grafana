969fe2e2125e28175fb697018191cf731515205b
Andy Taylor <andy0taylor@gmail.com>
2017-01-16 19:10:37 +0000

    typo

--- go.sh
+++ go.sh
@@ -4,7 +4,7 @@ chown -R grafana:grafana /var/lib/grafana /var/log/grafana
 
 # upgrade all installed plugins
 if [ "$UPGRADEALL" = true ] ; then
-    grafana-cli --pluginsDir "${GF_PLUGIN_DIR}" plugins upgrade-all || true
+    grafana-cli --pluginsDir "${PLUGIN_DIR}" plugins upgrade-all || true
 fi
 
 exec gosu grafana /usr/sbin/grafana-server \
@@ -12,5 +12,5 @@ exec gosu grafana /usr/sbin/grafana-server \
   --config=/etc/grafana/grafana.ini        \
   cfg:default.paths.data=/var/lib/grafana  \
   cfg:default.paths.logs=/var/log/grafana  \
-  cfg:default.paths.plugins=${GF_PLUGIN_DIR}
+  cfg:default.paths.plugins=${PLUGIN_DIR}
 
